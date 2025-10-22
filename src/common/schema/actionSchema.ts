6cc9589959a457fc1db963fc13333346d2355676
4geru <westhouse51@gmail.com>
2025-10-22 09:28:52 +0900

    change the z option

--- src/common/schema/actionSchema.ts
+++ src/common/schema/actionSchema.ts
@@ -9,22 +9,24 @@ import { z } from "zod";
 // 1. Postback action
 const postbackActionSchema = z.object({
   type: z.literal("postback"),
-  label: z.string().optional(),
+  label: z.string(),
   data: z.string(),
   displayText: z.string().optional(),
-  inputOption: z.enum(["closeRichMenu", "openRichMenu"]).optional(),
+  inputOption: z
+    .enum(["closeRichMenu", "openRichMenu", "openKeyboard", "openVoice"])
+    .optional(),
   fillInText: z.string().optional(),
 });
 // 2. Message action
 const messageActionSchema = z.object({
   type: z.literal("message"),
-  label: z.string().optional(),
+  label: z.string(),
   text: z.string(),
 });
 // 3. URI action
 const uriActionSchema = z.object({
   type: z.literal("uri"),
-  label: z.string().optional(),
+  label: z.string(),
   uri: z.string(),
   altUri: z
     .object({
@@ -35,7 +37,7 @@ const uriActionSchema = z.object({
 // 4. Datetime picker action
 const datetimePickerActionSchema = z.object({
   type: z.literal("datetimepicker"),
-  label: z.string().optional(),
+  label: z.string(),
   data: z.string(),
   mode: z.enum(["date", "time", "datetime"]),
   initial: z.string().optional(),
@@ -45,29 +47,29 @@ const datetimePickerActionSchema = z.object({
 // 5. Camera action
 const cameraActionSchema = z.object({
   type: z.literal("camera"),
-  label: z.string().optional(),
+  label: z.string()
 });
 // 6. Camera roll action
 const cameraRollActionSchema = z.object({
   type: z.literal("cameraRoll"),
-  label: z.string().optional(),
+  label: z.string()
 });
 // 7. Location action
 const locationActionSchema = z.object({
   type: z.literal("location"),
-  label: z.string().optional(),
+  label: z.string(),
 });
 // 8. Rich menu switch action
 const richMenuSwitchActionSchema = z.object({
   type: z.literal("richmenuswitch"),
-  label: z.string().optional(),
+  label: z.string(),
   richMenuAliasId: z.string(),
   data: z.string(),
 });
 // 9. Clipboard action
 const clipboardActionSchema = z.object({
   type: z.literal("clipboard"),
-  label: z.string().optional(),
+  label: z.string(),
   clipboardText: z.string(),
 });
 
